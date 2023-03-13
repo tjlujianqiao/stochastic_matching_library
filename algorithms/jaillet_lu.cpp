846fc5cdf7030a58b52444ebd5558470a3b80997
tjlujianqiao <57476783+tjlujianqiao@users.noreply.github.com>
2023-03-13 18:36:45 +0800

    nonintegral jl solution

--- algorithms/jaillet_lu.cpp
+++ algorithms/jaillet_lu.cpp
@@ -3,7 +3,7 @@ vector<vector<int>> graph::jaillet_lu_list()
 {
     int s = onSize + offSize, t = s + 1;
     flow_graph g(s, t);
-    
+
     for (int i = 0; i < onSize; i++)
         for (int j : adj[i])
             g.add_edge(i, j, 2);
@@ -17,24 +17,23 @@ vector<vector<int>> graph::jaillet_lu_list()
     g.max_flow();
 
     cycle_break_graph gCycle(onSize, onSize + offSize);
-    for (int i = 0; i < onSize; i ++)
+    for (int i = 0; i < onSize; i++)
         for (auto e : g.adj[i])
             if (e.flow > 0)
                 gCycle.add_edge(i, e.v, e.flow);
-            
+
     // Break cycles of type C2 and C3 required for theoretical guarantee
     gCycle.frac_to_int();
     gCycle.cycle_break();
 
     vector<vector<int>> res(onSize, vector<int>());
-    
-    for (int i = 0; i < onSize; i++) 
+
+    for (int i = 0; i < onSize; i++)
         for (auto e : gCycle.vInt[i])
             for (int j = 0; j < e.second; j++)
                 res[i].push_back(e.first);
     return res;
 }
-
 
 // Match with list of each online types
 vector<int> graph::jaillet_lu(vector<vector<int>> &jlList)
@@ -56,4 +55,39 @@ vector<int> graph::jaillet_lu(vector<vector<int>> &jlList)
         }
     }
     return res;
+}
+
+// Compute LP solution for non-integral algorithm in Jaillet and Lu (2013)
+map<pair<int, int>, double> graph::jaillet_lu_non_integral()
+{
+    // adding jb for each offline vertex j
+    int s = onSize + 2 * offSize, t = s + 1;
+    int mul = 1e9, ln = 306852819;
+    flow_graph g(s, t);
+
+    for (int i = 0; i < onSize; i++)
+        for (int j : adj[i])
+        {
+            int jb = j + offSize;
+            g.add_edge(i, j, mul / 2);
+            g.add_edge(i, jb, mul);
+            g.add_edge(jb, j, ln);
+        }
+
+    for (int i = 0; i < onSize; i++)
+        g.add_edge(s, i, mul);
+
+    for (int j = onSize; j < onSize + offSize; j++)
+        g.add_edge(j, t, mul);
+    g.max_flow();
+    map<pair<int, int>, double> jlProb;
+    for (int i = 0; i < onSize; i++)
+        for (auto e : g.adj[i])
+            if (e.flow > 0)
+            {
+                int j = (e.v < onSize + offSize) ? e.v : e.v - offSize;
+                jlProb[make_pair(i, e.v)] += (double)e.flow / mul;
+            }
+
+    return jlProb;
 }
